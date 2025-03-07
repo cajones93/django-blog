f5c1720e7a2d78d905b6d2b23e801e24309ef16d
Callum <callumjones93@hotmail.com>
2025-03-07 22:06:22 +0000

    added missing comma to prepopulated fields tuple

--- blog/admin.py
+++ blog/admin.py
@@ -8,7 +8,7 @@ class PostAdmin(SummernoteModelAdmin):
     list_display = ('title', 'slug', 'status')
     search_fields = ['title']
     list_filter = ('status',)
-    prepopulated_fields = {'slug': ('title')}
+    prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
 
 
