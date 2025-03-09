e72eeaab03afba924fb3c70cbea7e9a0ae614040
Callum <callumjones93@hotmail.com>
2025-03-09 08:47:08 +0000

    add CSS file

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,6 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
