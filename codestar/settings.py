77add8e21803bc5afe1c72a2218723e7d259041b
Callum <callumjones93@hotmail.com>
2025-03-09 08:39:21 +0000

    Revert "add index template"
    This reverts commit 3b86d4a92a466b34e60918c2986370a38b5bf190.
    
    revert changes

--- codestar/settings.py
+++ codestar/settings.py
@@ -59,13 +59,10 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'codestar.urls'
 
-TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
-
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
