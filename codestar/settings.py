3b86d4a92a466b34e60918c2986370a38b5bf190
Callum <callumjones93@hotmail.com>
2025-03-09 08:31:08 +0000

    add index template

--- codestar/settings.py
+++ codestar/settings.py
@@ -59,10 +59,13 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'codestar.urls'
 
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
