5733d3c97892b19f860130be485125525ab50704
Callum <callumjones93@hotmail.com>
2025-03-14 09:56:00 +0000

    added print statements to post_detail view

--- blog/views.py
+++ blog/views.py
@@ -31,7 +31,9 @@ def post_detail(request, slug):
     comment_count = post.comments.filter(approved=True).count()
 
     if request.method == "POST":
+        print("Received a POST request")
         comment_form = CommentForm(data=request.POST)
+
         if comment_form.is_valid():
             comment = comment_form.save(commit=False)
             comment.author = request.user
@@ -43,6 +45,7 @@ def post_detail(request, slug):
             )
 
     comment_form = CommentForm()
+    print("About to renter template")
 
     return render(
         request,
